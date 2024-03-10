d06e0e0bc10fb94c1390e81f339323044c4a681f
Vanilla Viking <ashwinr2k2@gmail.com>
2024-03-10 19:42:45 +1100

    working base code lab4

--- lab4/main.c
+++ lab4/main.c
@@ -1,5 +1,48 @@
 #include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+#include "usart.h"
+#include "bit.h"
+
+int setPrescaler_tc0(char option);
+void my_delay_us(unsigned long x);
+
+void my_delay_1e6us();
+
+volatile unsigned long numOV = 0;
+
+ISR(TIMER0_OVF_vect)
+{
+  numOV++;
+}
 
 int main(void) {
+    usart_init(8);
+    bitSet(TIMSK0, TOIE0);
+    sei();
 
+    while (1) {
+      usart_tx_string(">a:");
+      usart_transmit('1');
+      usart_transmit('\n');
+      my_delay_1e6us();
+
+      usart_tx_string(">a:");
+      usart_transmit('0');
+      usart_transmit('\n');
+      _delay_us(1e6);
+
+    }
+}
+
+void my_delay_1e6us() {
+  unsigned long numOV_max = 62500;
+
+  numOV = 0;
+  TCNT0 = 0;
+  bitSet(TCCR0B, CS00);
+  while (numOV < numOV_max -1);
+
+  bitClear(TCCR0B, CS00);
 }
